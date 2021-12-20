2beb48a2abe292c7dfc4fca969429d122aac209c
Tom <50991326+tomgeorgelin@users.noreply.github.com>
2021-12-20 16:51:25 +0100

    feat(server): add cors access

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://shortener-api-user:GO
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
