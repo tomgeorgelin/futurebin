953c19c59ac4777bb901954c793a1e8603ac85e1
Tom <50991326+tomgeorgelin@users.noreply.github.com>
2021-12-20 16:56:16 +0100

    feat: add Futurebin route

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./Navbar";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './Home'
+import Futurebin from "./Futurebin";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/:code" element={<Futurebin/>} />
       </Routes>
     </Router>
   );
